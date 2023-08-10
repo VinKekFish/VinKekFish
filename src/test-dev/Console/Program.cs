301181e0a60af10ee76baa4e5c9ff58d41af6f8d
fdsc <fdsc@yandex.ru>
2023-08-10 11:09:35 +0300

    Немного попробовал цвета в консоли

---
+++ src/test-dev/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ConsoleTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 24bit or truecolor
+        var tc = System.Environment.GetEnvironmentVariable("COLORTERM");
+        Console.WriteLine(tc);
+        Console.WriteLine($"isTrueColor: {isTrueColor(tc)}");
+        Console.WriteLine($"Эти цвета различны, если TrueColor поддерживается:");
+        // \x1b то же, что и echo -e "\033" в терминале
+        Console.WriteLine("\x1b[1m\x1b[48;2;255;0;0m\x1b[38;2;0;0;255mTRUECOLOR\x1b[0m\x1b[0m");
+        Console.WriteLine("\x1b[1m\x1b[48;2;224;0;0m\x1b[38;2;0;0;224mTRUECOLOR\x1b[0m\x1b[0m");
+        Console.WriteLine("\x1b[1m\x1b[48;2;192;0;0m\x1b[38;2;0;0;192mTRUECOLOR\x1b[0m\x1b[0m");
+    }
+
+    public static bool isTrueColor(string? COLORTERM)
+    {
+        if (COLORTERM == null)
+            return false;
+
+        COLORTERM = COLORTERM.ToLowerInvariant().Trim();
+        if (COLORTERM == "24bit" || COLORTERM == "truecolor")
+            return true;
+
+        return false;
+    }
+}
--- src/test-dev/README.md
+++ src/test-dev/README.md
@@ -1,3 +1,7 @@
 # [SecureCompare](SecureCompare)
 
 Тесты для VinKekFish_Utils.Utils.SecureCompare - это одноразовые тесты для тайминг-атак. Они не запускаются автоматически
+
+# [Console](Console)
+
+
