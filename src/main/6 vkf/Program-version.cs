fb7dfb54f718b798c51f08ab28e51e2cd1c48c8b
fdsc <fdsc@yandex.ru>
2023-09-10 09:49:00 +0300

    automatic commit

--- src/main/6 vkf/Program-version.cs
+++ src/main/6 vkf/Program-version.cs
@@ -2,5 +2,5 @@ namespace VinKekFish_console;
 
 public partial class Program
 {
-    public static readonly string ProgramVersion_RiWiWak6ObEcc ="2023.09.08.1847";
+    public static readonly string ProgramVersion_RiWiWak6ObEcc ="2023.09.09.1554";
 }
