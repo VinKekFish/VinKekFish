d180ffa9d431c467bc901ccf8f24ecb6b2521351
fdsc <fdsc@yandex.ru>
2024-02-01 09:06:00 +0300

    automatic commit

--- src/main/6 vkf/Program-version.cs
+++ src/main/6 vkf/Program-version.cs
@@ -2,5 +2,5 @@ namespace VinKekFish_console;
 
 public partial class Program
 {
-    public static readonly string ProgramVersion_RiWiWak6ObEcc ="2024.01.31.1857";
+    public static readonly string ProgramVersion_RiWiWak6ObEcc ="2024.01.31.2007";
 }
